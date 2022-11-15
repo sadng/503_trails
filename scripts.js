998296653a732c8f344faf63a8441c3957010f8b
sadng <115597343+sadng@users.noreply.github.com>
2022-11-15 14:37:43 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A'
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+center: [-103.2502, 29.2498], // starting position [lng, lat]
+zoom: 1.5, // starting zoom
+//pitch: 85,
+//bearing: 80, 
+});
